2b1ae1d6a7b58afddca65f88b06ebf8afbca4cd0
Karen.Jebakany <karen.jebakany@niit.com>
2022-08-05 18:08:22 +0530

    added demo solution

---
+++ src/main/java/com/jap/students/StudentDetails.java
@@ -0,0 +1,25 @@
+package com.jap.students;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class StudentDetails {
+
+    public static void main(String[] args) {
+        List<Integer> registrationsNumberList= Arrays.asList(242556,676767,871340,121234,753452,101223,456478);
+        System.out.println("The Registration List Before Sorting");
+        for (Integer i:registrationsNumberList
+             ) {
+            System.out.println(i);
+        }
+        Collections.sort(registrationsNumberList);
+        System.out.println("The Registration List After Sorting");
+        for (Integer i:registrationsNumberList
+        ) {
+            System.out.println(i);
+
+        }
+    }
+}
